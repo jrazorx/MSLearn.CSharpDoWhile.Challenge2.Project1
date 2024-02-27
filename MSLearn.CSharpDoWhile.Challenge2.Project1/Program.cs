e28cada9a7517439fa8fbae2e309f8134fa219f9
jra <julien.gault.50@gmail.com>
2024-02-27 18:13:31 +0100

    Test if not null and numeric, basic readline and loop

--- MSLearn.CSharpDoWhile.Challenge2.Project1/Program.cs
+++ MSLearn.CSharpDoWhile.Challenge2.Project1/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿string? readResult;
+int numericResult = 0;
+bool isNumberInput = false;
+bool isValidInput = false;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do
+{
+    readResult = Console.ReadLine();
+
+    if (readResult == null)
+        continue;
+    else
+    {
+        isNumberInput = int.TryParse(readResult, out numericResult);
+        Console.WriteLine(isNumberInput ? "C'est un numérique !" : "C'est pas un numérique ! :(");
+    }
+
+} while (isValidInput == false);
+
+//Console.WriteLine($"Your input value ({numericResult}) has been accepted.");
