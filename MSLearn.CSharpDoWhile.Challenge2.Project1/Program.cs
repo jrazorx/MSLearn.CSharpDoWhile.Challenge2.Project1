125b3c930e9986658f93e0b4a3b1f4920fe570fa
jra <julien.gault.50@gmail.com>
2024-02-27 18:22:59 +0100

    solution

--- MSLearn.CSharpDoWhile.Challenge2.Project1/Program.cs
+++ MSLearn.CSharpDoWhile.Challenge2.Project1/Program.cs
@@ -3,6 +3,14 @@ int numericResult = 0;
 bool isNumberInput = false;
 bool isValidInput = false;
 
+
+
+/*
+ * This code block asks the user to input a number between 5 and 10
+ * It checks if the input is a number and between 5 and 10
+ * Displays an error message if the input is invalid
+ * The loop will only exit upon valid input, and print a successful message
+*/
 Console.WriteLine("Enter an integer value between 5 and 10");
 
 do
@@ -12,11 +20,16 @@ do
     if (readResult == null)
         continue;
     else
-    {
         isNumberInput = int.TryParse(readResult, out numericResult);
-        Console.WriteLine(isNumberInput ? "C'est un numérique !" : "C'est pas un numérique ! :(");
-    }
+
+    if (!isNumberInput)
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    else if (isNumberInput && numericResult >= 5 && numericResult <= 10)
+        isValidInput = true;
+    else
+        Console.WriteLine($"You entered {numericResult}. Please enter a number between 5 and 10.");
+
 
 } while (isValidInput == false);
 
-//Console.WriteLine($"Your input value ({numericResult}) has been accepted.");
+Console.WriteLine($"Your input value ({numericResult}) has been accepted.");
